78bb268a2d37ae0565fdd48e7316d71823fa7b23
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-06-05 20:52:53 +0300

    added: router with main route

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const { PORT } = require("./config/config");
+const routes = require("./routes");
 
 const app = express();
+
 require("./config/database")();
+require("./config/express")(app);
+
+app.use(routes);
 
 app.listen(PORT, console.log(`Run server on Port:${PORT}`));
--- config/express.js
+++ config/express.js
@@ -2,5 +2,5 @@ const express = require("express");
 
 module.exports = (app) => {
   app.use(express.json());
-  app.use(express.urlencoded());
+  app.use(express.urlencoded({ extended: false }));
 };
